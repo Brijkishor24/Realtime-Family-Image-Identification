cd10e3c387daf056bcfc63da5b36307693771d8f
Brijkishor24 <71825478+Brijkishor24@users.noreply.github.com>
2020-11-26 14:25:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,8 +6,22 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/cVf1768tT/model.json",modelloaded);
+}
+function modelloaded(){
+    console.log("Model Loaded");
 }
 
 function draw() {
-    image(video,0,0,600,600);
+    image(video,0,0,600,600); 
+    classifier.classify(video,getresult);
 }
+function getresult(error,results) {
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("member_name").innerHTML=results[0].label;
+        document.getElementById("accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+   }
